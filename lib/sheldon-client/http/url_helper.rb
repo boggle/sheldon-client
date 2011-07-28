073eb4cdefb9b1af772ca983035073bb51a70d02
Adolfo Builes <builes.adolfo@googlemail.com>
2011-07-28 18:52:07 +0200

    staging stream mailer support

--- lib/sheldon-client/http/url_helper.rb
+++ lib/sheldon-client/http/url_helper.rb
@@ -83,6 +83,13 @@ class SheldonClient
       Addressable::URI.parse( SheldonClient.host + path )
     end
 
+    def user_stream_url(user_id, options = {})
+      path = "/stream/users/#{user_id.to_i}"
+      uri = Addressable::URI.parse( SheldonClient.host + path )
+      uri.query_values = stringify_fixnums( options.update(options) )
+      uri
+    end
+
     private
 
     def stringify_fixnums(hsh)
--- spec/client/url_helper_spec.rb
+++ spec/client/url_helper_spec.rb
@@ -164,4 +164,28 @@ describe SheldonClient::UrlHelper do
       uri.path.should == "/ids/connections"
     end
   end
+
+  context "user_stream_url" do
+    let(:user_id){2}
+
+    it "should create the user stream url when given user" do
+      user = SheldonClient::Node.new(id: user_id, type: :user)
+      uri  = user_stream_url user
+      uri.should be_a( Addressable::URI )
+      uri.path.should eq("/stream/users/#{user_id}")
+    end
+
+    it "should create the user stream url when given the user id" do
+      uri  = user_stream_url "2"
+      uri.should be_a( Addressable::URI )
+      uri.path.should eq("/stream/users/#{user_id}")
+    end
+
+    it "should add page and per_page to the user stream" do
+      uri  = user_stream_url "2", page: 1, per_page: 5
+      uri.should be_a( Addressable::URI )
+      uri.path.should eq("/stream/users/#{user_id}")
+      uri.query_values.should eq({"page" => "1", "per_page" => "5"})
+    end
+  end
 end
