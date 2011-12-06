bdc3a826b39f5de564f31779bf222d51963de36d
Adolfo Builes <builes.adolfo@googlemail.com>
2011-12-06 13:29:07 +0100

    Fix requests specs.

--- spec/requests/sheldon_client_requests_spec.rb
+++ spec/requests/sheldon_client_requests_spec.rb
@@ -14,7 +14,6 @@ describe SheldonClient do
   end
 
   before(:all) do
-    SheldonClient.log = true
     WebMock.allow_net_connect!
     SheldonClient.host = ENV['sheldon'] || "http://sheldon.staging.moviepilot.com:2311"
     SheldonClient.elastodon_host = ENV['elastodon'] || 'http://ci-staging01.moviepilot.com:9200'
@@ -25,15 +24,15 @@ describe SheldonClient do
   end
 
   describe "searching" do
-    it "should find a node on sheldon" do
+    it "finds a node on sheldon" do
       SheldonClient.search(title: "The Matrix", type: :movie).first.should_not be_nil
     end
 
-    it "should find an user on sheldon given his facebook's username" do
+    it "finds an user on sheldon given his facebook's username" do
       SheldonClient.search( username: 'Gonzo Gonzales' ).first.should_not be_nil
     end
 
-    it "should find an user on sheldon given his facebook id" do
+    it "finds an user on sheldon given his facebook id" do
       SheldonClient.search( facebook_ids: "100002398994863", type: :user ).first.should_not be_nil
     end
   end
@@ -53,24 +52,25 @@ describe SheldonClient do
       sleep 3
     end
 
-    it "should have created a node in sheldon" do
+    it "creates a node in sheldon" do
       @node.should be_a SheldonClient::Node
       @node.type.should eq(:movie)
       @node.payload[:title].should  eq(movie_title)
       @node.payload[:sandbox_id].should eq(sandbox_id)
     end
 
-    it "should get the node from sheldon" do
+    it "gets the node from sheldon" do
       sleep 3
       results = SheldonClient.search(title: movie_title, sandbox_id: sandbox_id )
       results.size.should eq(1)
       results.first.should be_a SheldonClient::Node
-
       results.first.id.should eq(@node.id)
     end
 
-    it "should update the node in sheldon" do
-      SheldonClient.update(@node, production_year: "1999").should eq(true)
+    it "updates the node in sheldon" do
+      @node[:production_year] = "1999"
+      @node.save.should eq(true)
+
       node = SheldonClient.node @node.id
       @node.should_not eq(node)
       node.payload[:title].should eq(movie_title)
@@ -78,7 +78,15 @@ describe SheldonClient do
       node.payload[:production_year].should eq("1999")
     end
 
-    it "should delete node in sheldon" do
+    it "replaces the resource when calling with update" do
+      SheldonClient.update(@node, production_year: "1999").should eq(true)
+      node = SheldonClient.node @node.id
+      node.payload[:title].should be nil
+      node.payload[:sandbox_id].should be nil
+      node.payload[:production_year].should eq("1999")
+    end
+
+    it "deletes node in sheldon" do
       SheldonClient.delete(@node).should eq(true)
     end
   end
@@ -109,7 +117,7 @@ describe SheldonClient do
       delete_test_nodes
     end
 
-    it "should create a connection between two nodes" do
+    it "creates a connection between two nodes" do
       @movie.should_not be_nil
       @gozno.should_not be_nil
 
@@ -120,12 +128,12 @@ describe SheldonClient do
       @connection.payload[:weight].should eq(0.5)
     end
 
-    it "should find the created connection in sheldon" do
+    it "finds the created connection in sheldon" do
       SheldonClient.connection(@connection.id).should eq(@connection)
       SheldonClient.connection(from: @gozno, to: @movie, type: :likes ).should eq(@connection)
     end
 
-    it "should delete a connection" do
+    it "deletes a connection" do
       SheldonClient.delete(connection: @connection.id).should eq(true)
 
       lambda{ SheldonClient.connection(@connection.id) }.should raise_error SheldonClient::NotFound
@@ -134,12 +142,12 @@ describe SheldonClient do
   end
 
   describe "getting all the ids of the nodes with the given type" do
-    it "should have several entries for users" do
+    it "has several entries for users" do
       users_ids = SheldonClient.all(:users)
       (users_ids.count > 100).should eq(true)
     end
 
-    it "should have more than a thousand of nodes" do
+    it "has more than a thousand of nodes" do
       nodes_ids = SheldonClient.all(:nodes)
       (nodes_ids.count > 1000).should eq(true)
     end
@@ -169,7 +177,7 @@ describe SheldonClient do
   #end
 
   describe "statistics" do
-    it "should have a thousands of movies" do
+    it "has a thousands of movies" do
       statistics = SheldonClient.statistics
       (statistics["nodes"]["movies"]["count"] > 1000 ).should eq(true)
     end
